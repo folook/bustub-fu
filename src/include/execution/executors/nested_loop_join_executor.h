283441ba544a0f7380023f117c79e1dbdbd6123f
folook <406544897@qq.com>
2023-06-14 11:09:47 +0800

    project3 task2 finish

--- src/include/execution/executors/nested_loop_join_executor.h
+++ src/include/execution/executors/nested_loop_join_executor.h
@@ -53,8 +53,28 @@ class NestedLoopJoinExecutor : public AbstractExecutor {
   auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); };
 
  private:
+  bool InnerJoin(const Schema &schema, Tuple *tuple);
+  bool LeftJoin(const Schema &schema, Tuple *tuple);
+
   /** The NestedLoopJoin plan node to be executed. */
   const NestedLoopJoinPlanNode *plan_;
+  std::unique_ptr<AbstractExecutor> left_child_;
+  std::unique_ptr<AbstractExecutor> right_child_;
+
+  std::vector<Tuple> right_tuples_;
+
+  //右表的游标
+  uint64_t index_{0};
+  //左表是否和右表有匹配，这个 flag 对于 left join 很重要，因为 left join 没有匹配就要填充空行
+  // 这个 flag 表示左行已经匹配了一个空行或者右行
+  // is_match_ = true;
+  bool is_match_{false};
+
+  //之所以把他高位成员变量是因为右表没有遍历完的时候，下次进来这个 executor 还要接着 index!=0遍历，但是 tuple 只会在 index==0 时拉取：left_child_->Next(&left_tuple_, &left_rid_)，所以要先存好
+  Tuple left_tuple_;
+  RID left_rid_;
+
+
 };
 
 }  // namespace bustub
